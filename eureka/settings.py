f8c1de912e56b9b975c98af1d00dd5ed662489d1
Ivan Boyarintsev <96628641+boyarivan06@users.noreply.github.com>
2024-01-11 17:29:43 +0300

    Update settings.py

--- eureka/settings.py
+++ eureka/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,10 +21,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-5tc@9j5!xz#2-mypab55#a=)a5xci5up(2+28r0o&21(r0m+*-'
+SECRET_KEY = os.environ['SECRET_KEY']
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 AUTH_USER_MODEL = 'main_site.User'

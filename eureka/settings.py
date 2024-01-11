1094f482e95c51a299bf3c4ccea7d3889a78f87e
Ivan Boyarintsev <96628641+boyarivan06@users.noreply.github.com>
2024-01-11 17:43:26 +0300

    Update settings.py

--- eureka/settings.py
+++ eureka/settings.py
@@ -26,7 +26,9 @@ SECRET_KEY = os.environ['SECRET_KEY']
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+    '0.0.0.0',
+]
 AUTH_USER_MODEL = 'main_site.User'
 
 # Application definition
